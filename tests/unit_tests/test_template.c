bcfc2ecfffa5e72edcd639c65dd442ac80933f1c
LeTamanoir <martin.saldinger@gmail.com>
2023-01-15 20:40:36 +0100

    [ADD] remove old unit_tests and add new template with makefile

---
+++ tests/unit_tests/test_template.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2022
+** int specifier test
+** File description:
+** tests for specifier %b
+*/
+
+#include <criterion/criterion.h>
+
+void template_test_function(void){return;}
+
+Test(template_test_function, template_test)
+{
+    cr_assert(1);
+}
