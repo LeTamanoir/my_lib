5aa09d089dbf34a4b19e6fcb169b77cb78c3a420
LeTamanoir <martin.saldinger@gmail.com>
2023-02-14 01:10:34 +0100

    [FIX] coding style

--- lib/my/my_list/sort.c
+++ lib/my/my_list/sort.c
@@ -21,7 +21,7 @@ static node_t *partition(
     node_t *pivot = end;
     node_t *i = start->prev;
 
-    for (node_t *j = start; j != end; j = j->next) {   
+    for (node_t *j = start; j != end; j = j->next) {
         if (cmp_fn(j->data, pivot->data) <= 0) {
             i = (i != NULL) ? i->next : start;
             my_swap(&i->data, &j->data, sizeof(void *));
